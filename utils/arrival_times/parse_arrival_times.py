ac7871aa66ac24530348712a3d069e72ec8ebee6
shun4midx <shun4midx@gmail.com>
2026-01-31 19:20:35 +0800

    Upload CSVs/ODTs

---
+++ utils/arrival_times/parse_arrival_times.py
@@ -0,0 +1,8 @@
+############################################
+# Copyright (c) 2026 Shun/翔海 (@shun4midx) #
+# Project: Taipei-MRT-Scheduler            #
+# File Type: Python file                   #
+# File: parse_arrival_times.py             #
+############################################
+
+# There are 12345, 6, 7, 67 as possible CSV names. They are for weekdays, Sat, Sun, Sat and Sun respectively.
