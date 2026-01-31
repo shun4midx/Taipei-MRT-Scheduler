8be15960883017b56ef45e6c0c1a8ca747287b47
shun4midx <shun4midx@gmail.com>
2026-01-31 19:12:45 +0800

    Extern consts

--- utils/consts.h
+++ utils/consts.h
@@ -6,5 +6,10 @@
  ****************************************** */
 
 #pragma once
+#include <unordered_map>
+#include <string>
+
 enum Language {zh, en, jp, kr};
 enum Line {R, O, G, BL, BR, Y};
+
+extern std::unordered_map<std::string, Line> LINES;
